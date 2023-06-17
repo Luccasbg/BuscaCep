3f4646186b61621af350702d4f1fb45a9b217e18
Luccas Guimarães <contato.luccasguimaraes@gmail.com>
2023-06-17 10:53:05 -0300

    Upload do projeto

--- buscacep/src/router/index.js
+++ buscacep/src/router/index.js
@@ -1,21 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from "../views/AboutView.vue"
 import BuscaCepView from "../views/BuscaCepView.vue"
 
 const routes = [
   {
     path: '/',
-    name: 'HomeView',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'AboutView',
-    component: AboutView
-  },
-  {
-    path: "/cep",
     name: "BuscaCepView",
     component: BuscaCepView
   }
